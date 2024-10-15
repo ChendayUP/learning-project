6887fc9bd64ce3b9548ef1aeb240a907143f409c
ChendayUP <godlikeonline@hotmail.com>
2024-10-15 16:37:46 +0800

    feat: add gitignore file

--- python/flask-demo/main.py
+++ python/flask-demo/main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, session, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World2!'
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(session)
+        print(request.form)
+        print('username', request.form['username'])
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username></p>
+            <p><input type=submit value=Login></p>
+        </form>
+    '''
 
 @app.route('/hello')
 def hello():
